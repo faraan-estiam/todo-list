b16168d1ff12402a5e3fa4067469b39c42754b29
faraan-estiam <147595360+faraan-estiam@users.noreply.github.com>
2024-05-09 00:14:03 +0200

    added firebase config and auth router

--- api/main.py
+++ api/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+
+# Documentation
 from documentation.description import api_description
 from documentation.tags import tags_metadata
+
+#Importing routes
+import routers.router_auth
 
 api = FastAPI(
   title="todo-list-api",
@@ -8,3 +13,5 @@ api = FastAPI(
   openapi_tags=tags_metadata,
   docs_url='/'
 )
+
+api.include_router(routers.router_auth.router)
